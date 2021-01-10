84e84622c52c93cf697b8082ba4151c18fb5ac15
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-01-09 21:37:14 -0600

    presupuesto state

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,7 +1,7 @@
 import React, {Fragment, useState} from 'react';
 import Error from './Error';
 
-const Pregunta = () => {
+const Pregunta = ({guardarPresupuesto, guardarRestante}) => {
 
     //definir state
     const [cantidad, guardarCantidad]= useState(0);
@@ -23,6 +23,8 @@ const Pregunta = () => {
 
         //Si se pasa la validacion
         guardarError(false);
+        guardarPresupuesto(cantidad);
+        guardarRestante(cantidad);
 
 
     }
