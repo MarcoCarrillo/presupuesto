3b3a26c1ed5be5595925d8e8e8b39b4fc8f67f0c
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-01-10 00:05:26 -0600

    Validar gastos

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,9 +1,39 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import Error from './Error';
 
 const Formulario = () => {
+
+    //state
+    const [nombre, guardarNombre ]= useState('');
+    const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
+ 
+    //cuando el user agrega un gasto
+
+    const agregarGasto = e =>{
+        e.preventDefault();
+
+        //validar
+        if(cantidad < 1 || isNaN(cantidad) || nombre.trim() === ''){
+            guardarError(true);
+            return;
+        }
+        //en caso de que pase la validacion ahora el error es false
+        guardarError(false);
+        //construir el gasto
+
+        //pasar el gasto al componente principal
+
+        //reset form
+    }
+
     return ( 
-        <form>
+        <form
+            onSubmit={agregarGasto}
+        >
             <h2>Agrega tus gastos aqui</h2>
+
+            {error ? <Error mensaje="Ambos campos son obligatorios o Presupuesto Incorrecto" /> : null }
 
             <div className="campo">
                 <label>Nombre del gasto</label>
@@ -11,6 +41,8 @@ const Formulario = () => {
                     type="text"
                     className="u-full-width"
                     placeholder="Ej. Transporte"
+                    value={nombre}
+                    onChange={e => guardarNombre(e.target.value)}
                 />
             </div>
 
@@ -20,6 +52,8 @@ const Formulario = () => {
                     type="number"
                     className="u-full-width"
                     placeholder="Ej. 200"
+                    value = {cantidad}
+                    onChange= {e=> guardarCantidad(parseInt( e.target.value))}
                 />
             </div>
 
