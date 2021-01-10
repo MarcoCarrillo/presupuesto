813bda3732def47643429bd237d9d9faef201f17
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-01-09 21:45:51 -0600

    formulario de gastos

---
+++ src/components/Formulario.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+
+const Formulario = () => {
+    return ( 
+        <form>
+            <h2>Agrega tus gastos aqui</h2>
+
+            <div className="campo">
+                <label>Nombre del gasto</label>
+                <input
+                    type="text"
+                    className="u-full-width"
+                    placeholder="Ej. Transporte"
+                />
+            </div>
+
+            <div className="campo">
+                <label>Cantidad del gasto</label>
+                <input
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Ej. 200"
+                />
+            </div>
+
+            <input
+                type="submit"
+                className="button-primary u-full-width"
+                value="Agregar Gasto"
+            />
+
+        </form>
+     );
+}
+ 
+export default Formulario;
